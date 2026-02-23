72a5c952166d2a4227fe128528d757f5114ba92e
nithinsettibathula <nithinsece58@gmail.com>
2026-02-23 15:16:39 +0530

    Task 9: Clean deployment from PowerShell

---
+++ main.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.82.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 1. Network Discovery (Using Default VPC)
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# 2. Security Group (Clean description for Task 9)
+resource "aws_security_group" "strapi_sg" {
+  name        = "nithin-strapi-sg-task9"
+  description = "Strapi traffic for Task 9"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 3. ECS Cluster
+resource "aws_ecs_cluster" "strapi_cluster" {
+  name = "nithin-strapi-cluster-task9"
+}
+
+# 4. Fargate Spot Capacity Provider
+resource "aws_ecs_cluster_capacity_providers" "provider" {
+  cluster_name       = aws_ecs_cluster.strapi_cluster.name
+  capacity_providers = ["FARGATE_SPOT"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 100
+  }
+}
+
+# 5. Task Definition (No CloudWatch Log Configuration)
+resource "aws_ecs_task_definition" "strapi_task" {
+  family                   = "nithin-strapi-task9"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  
+  # Using organization provided role
+  execution_role_arn       = "arn:aws:iam::811738710312:role/ecs_fargate_taskRole"
+  task_role_arn            = "arn:aws:iam::811738710312:role/ecs_fargate_taskRole"
+
+  container_definitions = jsonencode([{
+    name      = "strapi-container"
+    image     = "811738710312.dkr.ecr.us-east-1.amazonaws.com/nithin-strapi-repo:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 1337
+      hostPort      = 1337
+    }]
+    # CloudWatch logging removed as per Task 9 instructions
+  }])
+}
+
+# 6. ECS Service (Fargate Spot)
+resource "aws_ecs_service" "strapi_service" {
+  name            = "nithin-strapi-service-task9"
+  cluster         = aws_ecs_cluster.strapi_cluster.id
+  task_definition = aws_ecs_task_definition.strapi_task.arn
+  desired_count   = 1
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 1
+  }
+
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    assign_public_ip = true
+    security_groups  = [aws_security_group.strapi_sg.id]
+  }
+
+  depends_on = [aws_ecs_cluster_capacity_providers.provider]
+}
